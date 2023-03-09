c44555a99f55a1fc08cc31bcea1d47374e408c4a
AleHarm <101216320+AleHarm@users.noreply.github.com>
2023-03-09 12:43:37 -0700

    added turn system

--- play.js
+++ play.js
@@ -1,3 +1,11 @@
+var playerOneTurn = true;
+
 function clickHandler(e) {
     e.target.children[0].style.fill = "red";
+    //if(CURRENT PLAYER DIDN'T GET A SQUARE){
+        playerOneTurn = !playerOneTurn;
+        
+    //}else{
+        //DO NOTHING
+    //}
 }
