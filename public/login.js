caa2cdb8ddf8dfcbfaa362a60c81fb7d6b3e4c97
AleHarm <101216320+AleHarm@users.noreply.github.com>
2023-04-04 17:44:17 -0600

    added index.js

--- public/login.js
+++ public/login.js
@@ -1,14 +1,34 @@
-function loginUser() {
-  
+(async () => {
+  let authenticated = false;
+  const userName = localStorage.getItem('userName');
+  if (userName) {
+    const nameEl = document.querySelector('#userName');
+    nameEl.value = userName;
+    const user = await getUser(nameEl.value);
+    authenticated = user?.authenticated;
+  }
+
+  if (authenticated) {
+    document.querySelector('#playerName').textContent = userName;
+    setDisplay('loginControls', 'none');
+    setDisplay('playControls', 'block');
+  } else {
+    setDisplay('loginControls', 'block');
+    setDisplay('playControls', 'none');
+  }
+})();
+
+async function loginUser() {
+  loginOrCreate(`/api/auth/login`);
 }
 
-function createUser() {
-  oginOrCreate();
+async function createUser() {
+  loginOrCreate(`/api/auth/create`);
 }
 
 async function loginOrCreate(endpoint) {
-  const userName = document.querySelector('#Username')?.value;
-  const password = document.querySelector('#Password')?.value;
+  const userName = document.querySelector('#userName')?.value;
+  const password = document.querySelector('#userPassword')?.value;
   const response = await fetch(endpoint, {
     method: 'post',
     body: JSON.stringify({ email: userName, password: password }),
@@ -19,10 +39,13 @@ async function loginOrCreate(endpoint) {
   const body = await response.json();
 
   if (response?.status === 200) {
-    localStorage.setItem('Player', userName);
-    window.location.href = 'Dots-And-Dashes_PLAY.html';
+    localStorage.setItem('userName', userName);
+    window.location.href = 'play.html';
   } else {
-    console.log("Couldn't log in, need to create user");
+    const modalEl = document.querySelector('#msgModal');
+    modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+    const msgModal = new bootstrap.Modal(modalEl, {});
+    msgModal.show();
   }
 }
 
